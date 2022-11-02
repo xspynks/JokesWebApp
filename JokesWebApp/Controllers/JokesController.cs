a670572af96527cb2a6cc1ef47fe268f5bcaa6e6
Rafael de Paula <xspynks@gmail.com>
2022-11-02 20:46:52 -0300

    feat: Add search actions to the JokesController, create the view for the search

--- JokesWebApp/Controllers/JokesController.cs
+++ JokesWebApp/Controllers/JokesController.cs
@@ -26,6 +26,18 @@ namespace JokesWebApp.Controllers
                           View(await _context.Joke.ToListAsync()) :
                           Problem("Entity set 'ApplicationDbContext.Joke'  is null.");
         }
+        
+        // GET: Jokes/ShowSearchForm
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            return View();
+        }
+        
+        // POST: Jokes/ShowSearchResults
+        public async Task<IActionResult> ShowSearchResults(String SearchPhrase)
+        {
+            return View("Index", await _context.Joke.Where(j => j.JokeQuestion.Contains(SearchPhrase)).ToListAsync());
+        }
 
         // GET: Jokes/Details/5
         public async Task<IActionResult> Details(int? id)
