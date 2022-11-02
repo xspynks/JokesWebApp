07ca6e24e92d9ce50759cf4d36a88d9844910e6f
Rafael de Paula <xspynks@gmail.com>
2022-11-02 18:48:39 -0300

    first commit! :)

---
+++ JokesWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokesWebApp.Models;
+
+namespace JokesWebApp.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<JokesWebApp.Models.Joke>? Joke { get; set; }
+}
