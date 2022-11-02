2a16d5ffdaf7d48ac2cc3f019c048a273b88df51
Rafael de Paula <xspynks@gmail.com>
2022-11-02 19:46:02 -0300

    feat: creates the database and migrate the Jokes table

--- JokesWebApp/Data/Migrations/20221102220000_initialsetup.Designer.cs
+++ JokesWebApp/Data/Migrations/20221102220000_initialsetup.Designer.cs
@@ -3,16 +3,18 @@ using System;
 using JokesWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace JokesWebApp.Data.Migrations
+namespace JokesWebApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221102220000_initialsetup")]
+    partial class initialsetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.10");
